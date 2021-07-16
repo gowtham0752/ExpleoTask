9f0d38a525957abe08fad84a82f77ab2cde1dd24
gowtham <gowtham.n@rshop.co.in>
2021-07-16 13:36:57 +0530

    initial

--- app/src/main/java/com/expleo/task/Activity/MainActivity.kt
+++ app/src/main/java/com/expleo/task/Activity/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.expleo.task.Activity
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.expleo.task.Fragments.CameraFragment
 import com.expleo.task.R
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -33,7 +39,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val id = v?.id
         when(id){
             R.id.qr_card ->{
-
+                if (checkPermissionForCamera()){
+                    openQR()
+                }else{
+                    requestPermissionForCamera()
+                }
             }
             R.id.cantact_card ->{
 
@@ -41,6 +51,47 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.profile_card ->{
               startActivity(Intent(this, ProfilePictureActivity::class.java))
             }
+        }
+    }
+
+    fun openQR(){
+        Log.d("QR","---------------->QR Started")
+        val integrator = IntentIntegrator(this)
+        Log.d("QR","---------------->QR Initialized")
+        //integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES)
+        integrator.setPrompt("Scan a barcode")
+        integrator.setCameraId(0) // Use a specific camera of the device
+        integrator.setBeepEnabled(false)
+        integrator.setOrientationLocked(true)
+        integrator.setBarcodeImageEnabled(true)
+        integrator.initiateScan()
+
+    }
+
+    fun checkPermissionForCamera(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED )
+        } else false
+    }
+
+    fun requestPermissionForCamera() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA),
+                100
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(applicationContext,"No data found", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
